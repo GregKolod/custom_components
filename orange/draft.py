b553f3e652919a37a8a4ff71f5796263c21f7a85
GregKolod <40234158+GregKolod@users.noreply.github.com>
2020-03-08 21:33:32 +0100

    working version but without async get

--- orange/draft.py
+++ orange/draft.py
@@ -1,20 +1,37 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
+# from urllib.request import urlopen
+# from bs4 import BeautifulSoup
+#
+# BASE_URL = 'https://www.teleman.pl/program-tv/stacje/'
+#
+# page = urlopen(BASE_URL)
+# soup = BeautifulSoup(page, 'html.parser')
+#
+# tv_stacje = soup.find('div', attrs={'id': 'stations-index'}).find_all('a')
+#
+# # zwraca nazwe stacji do przygotowania linku dla scrapera
+#
+# for stacja in enumerate(tv_stacje):
+#     link_tv = str(stacja[1]['href']).split('/')[3]
+#     nazwa_stacji = stacja[1].text
+#     print(nazwa_stacji, link_tv)
+#     # print(BASE_URL+link_tv)
+#
 
-BASE_URL = 'https://www.teleman.pl/program-tv/stacje/'
+import requests
+from io import BytesIO
+from PIL import Image
 
-page = urlopen(BASE_URL)
-soup = BeautifulSoup(page, 'html.parser')
+img_url = 'https://media.teleman.pl/photos/Anglik-Ktory-Mnie-Kochal.jpeg'
+img_url = ''
+basewidth = 300
+response = requests.get(img_url)
+img = Image.open(BytesIO(response.content))
 
-tv_stacje = soup.find('div', attrs={'id': 'stations-index'}).find_all('a')
+wpercent = (basewidth / float(img.size[0]))
+hsize = int((float(img.size[1]) * float(wpercent)))
+img = img.resize((basewidth, hsize), Image.ANTIALIAS)
 
-# zwraca nazwe stacji do przygotowania linku dla scrapera
-
-for stacja in enumerate(tv_stacje):
-    link_tv = str(stacja[1]['href']).split('/')[3]
-    nazwa_stacji = stacja[1].text
-    print(nazwa_stacji, link_tv)
-    # print(BASE_URL+link_tv)
+img.show()
